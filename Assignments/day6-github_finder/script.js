a65a97fef80c4698cc4ec68c96013a67d961a8f0
Sohee Lee <essen030216@naver.com>
2025-10-01 20:51:46 +0900

    feat: 사용자 프로필 조회 기능 추가

--- Assignments/day6-github_finder/script.js
+++ Assignments/day6-github_finder/script.js
@@ -1,10 +1,6 @@
 const usernameInput = document.getElementById("username");
 
 usernameInput.addEventListener("input", async (e) => {
-    //깃허브 api 연동해서.... username에 해당하는 사용자 정보 불러오기
-    //(프로필 정보 / 레포 정보)
-
-    //만약 대응하는 username이 없을 시 wrap-container 상단 구역에 User not found div 나타나도록.
     const username = e.target.value.trim();
 
     if (username.length == 0) {
@@ -13,14 +9,42 @@ usernameInput.addEventListener("input", async (e) => {
     }
     try {
         //Github api
+        //fetch함수 안에 api를 ....
         const response = await fetch(`https://api.github.com/users/${username}`);
-        if (!response.ok) {
-            //User non found div 만들기
-        }
-        const data = await response.json();
 
-        //#profile에 프로필 정보,
-        //#repos에 레포 정보 보여주기
+        if (!response.ok) {  //fetch api에서 반환된 응답(객체)에 있는 속성 - boolean값
+            document.getElementById("result-container").innerHTML = `<p id="error-message">User not found</p>`;
+            document.getElementById("repos").innerHTML = "";
+            return;
+        }
+
+        //response.ok가 true면
+        const data = await response.json();    
+        //응답을 읽어서 json을 js객체로 파싱 -> response.json()가 promise라서 await 붙여야함.
+        //body를 JSON 객체로 변환해서 data에 담는 과정
+        console.log(data);
+
+        const profileDiv = document.getElementById("profile");
+        profileDiv.innerHTML = `
+            <div class="user-card">
+                <img src="${data.avatar_url}" alt="${data.login}" width="200" style="border-radius: 2px;"/>
+                <div id="user-info">
+                    <h2 id="user-id">${data.login}${data.name ? ` (${data.name})` : ""}</h2>
+                    <div id="info">
+                        <p>Follwer: ${data.followers} | Following: ${data.following}</p>
+                        <p>bio: ${data.bio || ".."}</p>
+                        <p>company: ${data.company || ".."}</p>
+                        <br>
+                        <p>Public Repos: ${data.public_repos}</p>
+                    </div>
+                </div>
+            </div>
+        `
+
+
+
+    }
+    catch {
 
     }
 });
